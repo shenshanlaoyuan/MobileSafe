532fe63172465f98573593cce7c8a9840219118f
shenshanlaoyuan <shenshanlaoyuan@gmail.com>
2016-08-30 16:53:03 +0800

    完成远程清除数据和远程报警功能

--- src/com/shenshanlaoyuan/mobilesafe/service/LostFindService.java
+++ src/com/shenshanlaoyuan/mobilesafe/service/LostFindService.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsMessage;
@@ -13,6 +15,7 @@ import android.telephony.SmsMessage;
 public class LostFindService extends Service {
 
 	private SmsReceiver smsReceiver;
+	private boolean isPlay;// false 音乐播放的标记
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -33,6 +36,9 @@ public class LostFindService extends Service {
 			for (Object data : datas) {
 				SmsMessage sm = SmsMessage.createFromPdu((byte[]) data);
 
+				// 获取设备管理器
+				DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(context.DEVICE_POLICY_SERVICE);
+
 				String messageBody = sm.getMessageBody();
 				if (messageBody.equals("#*gps*#")) {
 					// 获取去定位信息
@@ -41,13 +47,40 @@ public class LostFindService extends Service {
 					startService(service);// 启动定位的服务
 					abortBroadcast();// 终止广播
 				} else if (messageBody.equals("#*lockscreen*#")) {
-					//一键锁屏
-					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(context.DEVICE_POLICY_SERVICE);
-					//设置锁屏密码
+					// 一键锁屏
+
+					// 设置锁屏密码
 					dpm.resetPassword("123", 0);
 					dpm.lockNow();
-				} else {
+				} else if (messageBody.equals("#*wipedata*#")) {
+					// 远程清除数据
+					dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+					abortBroadcast();// 终止广播
 
+				} else if (messageBody.equals("#*music*#")) {
+					// 只播放一次
+					abortBroadcast();
+					if (isPlay) {
+						return;
+					}
+
+					// 远程报警
+					// 播放音乐
+					MediaPlayer mp = MediaPlayer.create(
+							getApplicationContext(),
+							com.shenshanlaoyuan.mobilesafe.R.raw.qqqg);
+					// 设置左右声道声音为最大值
+					mp.setVolume(1, 1);
+					mp.start();// 开始播放
+					mp.setOnCompletionListener(new OnCompletionListener() {
+						
+						@Override
+						public void onCompletion(MediaPlayer mp) {
+							//音乐播放完毕，触发此方法
+							isPlay = false;
+						}
+					});
+					isPlay = true;
 				}
 
 			}
